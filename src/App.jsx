a6775b9dffb2742484d196fd1fbc85b01074cd63
Mihailo Markovic <markovic.mihailo98@gmail.com>
2025-07-07 15:14:10 +0200

    Add global syles for different headings

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -19,9 +14,13 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>hello!</H1>
+        <Heading as="h1">hello!</Heading>
+        <Heading as="h2">checking</Heading>
+
         <Button onClick={() => alert("Check in")}>Check it</Button>
         <Button onClick={() => alert("Check out")}>Check out</Button>
+
+        <Heading as="h3">Form</Heading>
         <Input type="number" placeholder="Number of guests" />
       </StyledApp>
     </>
