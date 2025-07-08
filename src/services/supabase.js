6ca2023a7e34832b01cbd22706bee8a2cca3404a
Mihailo Markovic <markovic.mihailo98@gmail.com>
2025-07-08 14:39:18 +0200

    Create database via superbase platform and implement into project

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://rjxekyjcnpqgobihkkon.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqeGVreWpjbnBxZ29iaWhra29uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5NzQwMjUsImV4cCI6MjA2NzU1MDAyNX0.gKRidNBcTNAQVLoY4Sg-GRE7L9Fn3BHAyvNb4N9Biv4";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
