c0fbff42ed4fee463735f9a2548750a5c623ffb3
Mihailo Markovic <markovic.mihailo98@gmail.com>
2025-07-12 15:44:25 +0200

    Extract outsideClick funstion in reusable custom hook

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -9,6 +9,7 @@ import {
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import { useOutSideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -82,20 +83,7 @@ function Open({ children, opens: opensWindowsName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
-  const ref = useRef();
-
-  useEffect(
-    function () {
-      function handleClick(e) {
-        if (ref.current && !ref.current.contains(e.target)) close();
-      }
-
-      document.addEventListener("click", handleClick, true);
-
-      return () => document.removeEventListener("click", handleClick, true);
-    },
-    [close]
-  );
+  const ref = useOutSideClick(close);
 
   if (name !== openName) return null;
 
