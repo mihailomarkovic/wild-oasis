285fbe26f79b54d632e96c05f91e415107682679
Mihailo Markovic <markovic.mihailo98@gmail.com>
2025-07-14 11:59:47 +0200

    Build reusable Table body using render props

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -88,10 +88,14 @@ function Row({ children }) {
   );
 }
 
-function Body({ children }) {}
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
+
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
 
 Table.Header = Header;
-Table.Footer = Footer;
+Table.Body = Body;
 Table.Row = Row;
 Table.Footer = Footer;
 
