ef91e069e411ec34a76ce49437260ed6784db774
Mihailo Markovic <markovic.mihailo98@gmail.com>
2025-08-05 15:18:54 +0200

    Implement delete functionality

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -41,6 +41,7 @@ function Filter({ filterField, options }) {
 
   function handleClick(value) {
     searchParams.set(filterField, value);
+    if (searchParams.get("page")) searchParams.set("page", 1);
     setSearchParams(searchParams);
   }
 
@@ -50,7 +51,7 @@ function Filter({ filterField, options }) {
         <FilterButton
           key={option.value}
           onClick={() => handleClick(option.value)}
-          $active={option.value === currentFilter}
+          active={option.value === currentFilter}
           disabled={option.value === currentFilter}
         >
           {option.label}
